b14077d2342be1fb54860f5c39841bc4e790c290
BB545 <black1016@hanmail.net>
2024-05-03 11:15:42 +0900

    public 경로 수정 및 index.ejs 렌더링

--- app.js
+++ app.js
@@ -6,13 +6,12 @@ const Iamport = require('iamport');
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
-// 미들웨어 설정
-app.use(bodyParser.json()); // JSON 형태의 본문 분석을 위해
-app.use(bodyParser.urlencoded({ extended: true })); // URL-encoded 형태의 본문 분석을 위해
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', (req,res)=>{
-    res.render('test')
+    res.render('index')
 })
 
 app.listen(8000, () => {
