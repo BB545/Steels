08b7c76bfed3948fdbbbf808af112911cbd059b4
BB545 <black1016@hanmail.net>
2024-05-03 12:43:00 +0900

    상품 페이지 추가 및 경로 수정

--- controller/Cmain.js
+++ controller/Cmain.js
@@ -3,3 +3,19 @@
 exports.main = (req,res) => {
     res.render('index')
 }
+
+exports.productAll = (req,res) => {
+    res.render('productAll')
+}
+
+exports.productLiving = (req,res) => {
+    res.render('productLiving')
+}
+
+exports.productOut = (req,res) => {
+    res.render('productOut')
+}
+
+exports.productLife = (req,res) => {
+    res.render('productLife')
+}
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const controller = require('../controller/Cmain')
 
 router.get('/', controller.main)
+router.get('/productAll', controller.productAll)
+router.get('/productLiving', controller.productLiving)
+router.get('/productOut', controller.productOut)
+router.get('/productLife', controller.productLife)
 
 module.exports = router;
